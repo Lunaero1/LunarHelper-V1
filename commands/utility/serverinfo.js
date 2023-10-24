e9c6553eeda7b828e29511e6f198683f94af17a3
Lunaero1 <johnrules102@gmail.com>
2023-10-24 14:26:20 -0500

    ...

--- commands/utility/serverinfo.js
+++ commands/utility/serverinfo.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,9 +8,17 @@ module.exports = {
     async execute(interaction) {
         const guild = interaction.guild;
         const memberCount = guild.memberCount;
-        const serverOwner = guild.ownerId;
-        const serverCreationDate = guild.createdAt.toDateString();
+        const serverOwner = guild.owner;
+        const serverCreationDate = guild.createdTimestamp;
 
-        await interaction.reply(`Server Name: ${guild.name}\nMember Count: ${memberCount}\nServer Owner: ${serverOwner}\nServer Creation Date: ${serverCreationDate}`);
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('Server Information')
+            .addFields('Server Name', guild.name)
+            .addFields('Member Count', memberCount)
+            .addFields('Server Owner', `${serverOwner.user.username}#${serverOwner.user.discriminator}`)
+            .addFields('Server Creation Date', new Date(serverCreationDate).toDateString());
+
+        await interaction.reply({ embeds: [embed] });
     },
 };
